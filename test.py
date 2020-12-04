25a921e61e0dbcacde19a4a7569a640e305f01a1
mw19970811 <1471851278@qq.com>
2020-12-04 10:09:21 +0800

    third commit

--- test.py
+++ test.py
@@ -22,3 +22,15 @@ class Test(torch.nn.Module):
 
     def forward(self, inputA, inputB):
         return TestFunction.apply(inputA, inputB)
+
+if __name__ == "__main__":
+    x = torch.autograd.Variable(torch.Tensor([1,2,3]), requires_grad=True)
+    y = torch.autograd.Variable(torch.Tensor([4,5,6]), requires_grad=True)
+    test = Test()
+    z = test(x, y)
+    z.sum().backward()
+    print('x: ', x)
+    print('y: ', y)
+    print('z: ', z)
+    print('x.grad: ', x.grad)
+    print('y.grad: ', y.grad)
